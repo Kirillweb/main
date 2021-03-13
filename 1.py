ffeaf16f9a49c8d9e9bdbcf6bbf171306e037e05
Kirillweb <80586806+Kirillweb@users.noreply.github.com>
2021-03-13 17:09:59 +0300

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,7 @@
+echo "# main" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Kirillweb/main.git
+git push -u origin main
